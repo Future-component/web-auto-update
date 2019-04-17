702ee393967f944cd3f3c3c93469e1ae2bc03a51
admin <admin@admindeMacBook-Pro.local>
2019-04-17 10:04:21 +0800

    添加接口调用和返回处理

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
 import axios from 'axios'
 
+axios.defaults.withCredentials = true;
+
 class AutoUpdate {
   constructor(options) {
     axios({
-      url: options.host + '/stat/version',
-    })
-  }
+      url: `${options.host}/stat/version`,
+      method: 'GET',
+    }).then((response) => {
+      console.log('response ', response)
+      let errMsg
+      if (response.status >= 200 && response.status < 300) {
+        if (response.data.code === 0) {
+          const newVersion = Number(`${response.data.data}`.replace('.', ''))
+          const oldVersion = Number(`${options.version}`.replace('.', ''))
+          console.log(response.data.data, options.version, newVersion, oldVersion, '???', newVersion > oldVersion)
+          if (newVersion > oldVersion) {
+            window.location.reload()
+          }
+          return response.data
+        }
+        errMsg = response.data
+      } else {
+        errMsg = response.statusText
+      }
 
-  run() {
-    window.location.reload()
+      const error = new Error(errMsg)
+      error.response = response
+      error.msg = errMsg
+      throw error
+    }).catch((err) => {
+      console.log('接口报错 ', err)
+      let msgTmp = err
+      const data = err.response.data
+      if (err.response) {
+        msgTmp = `${err.response.data.code}_${err.response.data.msg}`
+      } else {
+        msgTmp = err.message || err.msg
+      }
+      return { error: { msg: msgTmp, data } }
+    })
   }
 }
 
